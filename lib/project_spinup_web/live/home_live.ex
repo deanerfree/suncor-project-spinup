e48c52ba2ddc0276870023fbdc3ee285c3bd439b
Kurt Ball <kurtisdball@hotmail.com>
2026-03-08 04:49:11 +0000

    add an icon for styling

--- lib/project_spinup_web/live/home_live.ex
+++ lib/project_spinup_web/live/home_live.ex
@@ -89,9 +89,28 @@ defmodule ProjectSpinupWeb.HomeLive do
             <div id="file-list" class="flex flex-col justify-center w-full gap-2">
               <%= for entry <- @uploads.pdf.entries do %>
                 <div class="flex items-center justify-center gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="ionicon text-gray-400" width="16" height="18" viewBox="0 0 512 512">
-                    <path d="M416 221.25V416a48 48 0 01-48 48H144a48 48 0 01-48-48V96a48 48 0 0148-48h98.75a32 32 0 0122.62 9.37l141.26 141.26a32 32 0 019.37 22.62z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="64"/>
-                    <path d="M256 56v120a32 32 0 0032 32h120" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="64"/>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="ionicon text-gray-400"
+                    width="16"
+                    height="18"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      d="M416 221.25V416a48 48 0 01-48 48H144a48 48 0 01-48-48V96a48 48 0 0148-48h98.75a32 32 0 0122.62 9.37l141.26 141.26a32 32 0 019.37 22.62z"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linejoin="round"
+                      stroke-width="64"
+                    />
+                    <path
+                      d="M256 56v120a32 32 0 0032 32h120"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="64"
+                    />
                   </svg>
                   <span class="font-medium text-lg text-gray-400">{entry.client_name}</span>
                   <button
@@ -132,7 +151,6 @@ defmodule ProjectSpinupWeb.HomeLive do
             Upload to Server
           </button>
         </form>
-
 
         <%= if @result do %>
           <%= for page <- @result do %>
